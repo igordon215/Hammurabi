463b744517be2067c0c30639167cfea9aaa6141f
igordon215 <igordon215@gmail.com>
2024-07-13 13:41:40 -0400

    lunch

--- docs/matuszek/playGame.java
+++ docs/matuszek/playGame.java
@@ -6,7 +6,28 @@ public class playGame {
         game.run();
     }
     public void run(){
-        
+        Hammurabi hammurabi = new Hammurabi();
+        int year = 1;
+        final int GAME_LENGTH = 10;
+
+        System.out.println("welcome msg");
+        System.out.println("starter msg");
+
+        while (year <= GAME_LENGTH){
+            System.out.printf("year");
+            System.out.printf("pop");
+            System.out.println("acres");
+            System.out.println("bushels");
+            System.out.printf("land_value");
+
+            //player choice
+
+            int acresToBuy=
+            int acresToSell=
+            int bushelsToFeed=
+            int acresToPlant=
+        }
+
     }
 
 
