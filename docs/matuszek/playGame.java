2747ac563f044cb7ba1f314d1e0b5f5e80bd5a4a
igordon215 <igordon215@gmail.com>
2024-07-13 16:46:39 -0400

    starting playGame

--- docs/matuszek/playGame.java
+++ docs/matuszek/playGame.java
@@ -5,7 +5,7 @@ public class playGame {
         playGame game = new playGame();
         game.run();
     }
-    public void run(){
+    public void run() {
         Hammurabi hammurabi = new Hammurabi();
         int year = 1;
         final int GAME_LENGTH = 10;
@@ -13,29 +13,67 @@ public class playGame {
         System.out.println("welcome msg");
         System.out.println("starter msg");
 
-        while (year <= GAME_LENGTH){
-            System.out.printf("year");
-            System.out.printf("pop");
+        while (year <= GAME_LENGTH) {
+            System.out.println("year");
+            System.out.println("pop");
             System.out.println("acres");
             System.out.println("bushels");
-            System.out.printf("land_value");
+            System.out.println("land_value");
 
             //player choice
 
-            int acresToBuy=
-            int acresToSell=
-            int bushelsToFeed=
-            int acresToPlant=
+            int acresToBuy = askAcresToBuy(hammurabi);
+            int acresToSell = askAcresToSell(hammurabi);
+            int bushelsToFeed = askBushelsToFeed(hammurabi);
+            int acresToPlant = askAcresToPlant(hammurabi);
+
+            // turn
+
+            processTurn(hammurabi, acresToBuy, acresToSell, bushelsToFeed, acresToPlant);
+
+            //game_voer
+
+            if (hammurabi.population <= 0) {
+                System.out.println("pop die");
+                break;
+
+                //conditions
+            }
+            if (hammurabi.uprising(hammurabi.population, hammurabi.starvationDeaths(hammurabi.population, bushelsToFeed))) {
+                System.out.println("People Revolt");
+                break;
+            }
+            year++;
+            hammurabi.landValue = hammurabi.newCostOfLand();
+
         }
+            if (year GAME_LENGTH){
+                //WIN
+            //
+            //
+            //
+        }
+            private int askAcresToBuy(){
 
+        }
+            private int askAcresToSell(){
+
+        }
+            private int askBushelsToFeed90{
+
+        }
+            private int askAcresToPlant{
+
+        }
+            private void processTurn90{
+                //buy/sell land
+                //feed people
+                // plant crops
+                
+
+                // calc results
+        }
     }
-
-
-
-
-
-
-
 
 
 
