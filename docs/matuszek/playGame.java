388485efe0c04382ffe0bff384e615dd6ecbd5d3
igordon215 <igordon215@gmail.com>
2024-07-13 13:24:12 -0400

    start play class

---
+++ docs/matuszek/playGame.java
@@ -0,0 +1,27 @@
+package hammurabi.docs.matuszek;
+
+public class playGame {
+    public static void main(String[] args){
+        playGame game = new playGame();
+        game.run();
+    }
+    public void run(){
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
