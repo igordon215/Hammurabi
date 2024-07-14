201076e19ad15e440229cabc517540a4ec1c1db9
igordon215 <igordon215@gmail.com>
2024-07-14 00:20:29 -0400

    added 0 check to immigrants method

--- docs/matuszek/Hammurabi.java
+++ docs/matuszek/Hammurabi.java
@@ -47,11 +47,16 @@ public class Hammurabi {         // must save in a file named Hammurabi.java
     // If everyone is well fed, compute how many people come to the city as:
     // (20 * _number of acres you have_ + _amount of grain you have in storage_) / (100 * _population_) + 1
     public int immigrants(int population, int bushels, int acresOwned) {
-        if (starvationDeaths(population, bushels) > 0){
+        if (population <= 0 || starvationDeaths(population, bushels) > 0) {
             return 0;
         }
         return (acresOwned + bushels) / (population * 4) - 17;
     }
+//        if (starvationDeaths(population, bushels) > 0){
+//            return 0;
+//        }
+//        return (acresOwned + bushels) / (population * 4) - 17;
+//    }
     //Choose a random integer between 1 and 6, inclusive. Each acre that was planted with seed will yield this many bushels of grain.
     //(Example: if you planted 50 acres, and your number is 3, you harvest 150 bushels of grain). Return the number of bushels harvested.
     public int harvest(int acresOwned) {
